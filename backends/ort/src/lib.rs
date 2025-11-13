b3e00f98d1d8c145080e357587f1fca48a215f96
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2025-11-13 10:42:09 +0100

    Fix config parsing

--- backends/ort/src/lib.rs
+++ backends/ort/src/lib.rs
@@ -20,11 +20,11 @@ pub struct Config {
     // as input i.e., whenever the ONNX model has been exported with optimized MHA/MQA nodes
     // NOTE: The renames from `n_embd`, `n_layer`, and `n_head` have been included for some edge
     // cases as e.g. `nomic-ai/nomic-embed-text-v1`, given that those ONNX exports use MQA
-    #[serde(rename = "n_embd")]
+    #[serde(rename = "n_embd", alias = "hidden_size")]
     pub hidden_size: usize,
-    #[serde(rename = "n_layer")]
+    #[serde(rename = "n_layer", alias = "num_hidden_layers")]
     pub num_hidden_layers: usize,
-    #[serde(rename = "n_head")]
+    #[serde(rename = "n_head", alias = "num_key_value_heads")]
     pub num_key_value_heads: Option<usize>,
 }
 
